c7e9f08ca05ae9cf6cc5407a2b85e1298fe28064
DoQuangHuy <huydo@hivelab.com>
2022-06-24 16:50:54 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { component } from 'react'
+import React from 'react'
 import Content from './Component/Content/Content.js';
+import Footer from './Component/Footer/Footer.js';
 import Header from './Component/Header/Header.js';
 import TopMenu from './Component/TopMenu/TopMenu.js';
 
@@ -9,17 +10,20 @@ function App() {
     {
       title: 'For those about to rock...',
       content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.',
-      src: 'assets/img/01.jpg'
+      src: 'assets/img/01.jpg',
+      order: 'order-lg-1',
     },
     {
       title: 'We salute you!',
       content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.',
-      src: 'assets/img/02.jpg'
+      src: 'assets/img/02.jpg',
     },
     {
       title: 'Let there be rock!',
       content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.',
-      src: 'assets/img/03.jpg'
+      src: 'assets/img/03.jpg',
+      order: 'order-lg-2',
+      order2: 'order-lg-1'
     },
 
   ]
@@ -28,10 +32,17 @@ function App() {
       <TopMenu />
       <Header />
       { 
-        contents.map((item, index) => {
-          <Content key={index} />
-        })
+        contents.map((item, index) => (
+          <Content 
+            key={index}
+            title={item.title} 
+            content={item.content}
+            src={item.src}
+            order={item.order}
+          />
+        ))
       }
+      <Footer />
     </div>
   );
 }
--- src/Component/Content/Content.js
+++ src/Component/Content/Content.js
@@ -1,17 +1,60 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 
 function Content( props ) {
+
+  const [state, setState] = useState(0)
+
+  const renderForm = () => (
+    <div className='row'>
+      <div className='form-group'>
+        <input 
+          type='text' 
+          name='name' 
+          className='form-control'
+          defaultValue={ props.title }
+        />
+        <div className='btn btn-block btn-danger mt-2' onClick={() => handleClickSave()}>Save</div>
+      </div>
+    </div>
+  )
+
+  const renderBtn = () => (
+    <div className='row'>
+      <div className='btn btn-group'>
+        <div className='btn btn-info' onClick={() => handleClickEdit()}>Edit</div>
+        <div className='btn btn-warning'>Remove</div>
+      </div>
+    </div>
+  )
+
+  const checkDisplay = () => {
+    if (state !== 0) {
+      return renderForm()
+    } else {
+      return renderBtn()
+    }
+  }
+
+  const handleClickEdit = () => {
+    setState(1);
+  }
+
+  const handleClickSave = () => {
+    setState(0);
+  }
+
   return ( 
     <section id="scroll">
       <div className="container px-5">
         <div className="row gx-5 align-items-center">
-          <div className="col-lg-6 order-lg-2">
+          <div className={"col-lg-6 " + props.order }>
             <div className="p-5"><img className="img-fluid rounded-circle" src={ props.src } alt="..." /></div>
           </div>
-          <div className="col-lg-6 order-lg-1">
+          <div className="col-lg-6">
             <div className="p-5">
               <h2 className="display-4">{ props.title }</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
+              <p>{ props.content }</p>
+              {checkDisplay()}
             </div>
           </div>
         </div>
